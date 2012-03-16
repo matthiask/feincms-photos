e1c76b58e454bc2c64c747add764bfc5a79b1b2e
Matthias Kestenholz <mk@spinlock.ch>
2012-03-16 15:45:54 +0100

    raw_id_fields improves your day

--- photos/admin.py
+++ photos/admin.py
@@ -5,9 +5,11 @@ from . import models
 
 admin.site.register(models.Album,
     list_display=('title', 'created', 'created_by'),
+    raw_id_fields=('created_by',),
     )
 
 admin.site.register(models.Photo,
     list_display=('title', 'photo', 'is_flagged'),
     list_filter=('is_flagged',),
+    raw_id_fields=('created_by', 'album'),
     )
