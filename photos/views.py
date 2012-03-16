df3900c2047494d602598f0c85ff0d58250d7509
Matthias Kestenholz <mk@spinlock.ch>
2012-03-16 09:38:57 +0100

    Hide created_by in forms

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,24 @@
 from towel import modelview
 
 
-class AlbumModelView(modelview.ModelView):
+class ModelView(modelview.ModelView):
+    """
+    ModelView subclass holding behavior specific to the photos app.
+    """
+
+    def get_form(self, request, instance=None, **kwargs):
+        return super(ModelView, self).get_form(request, instance=instance,
+            exclude=('created_by',))
+
+    def save_model(self, request, instance, form, change):
+        if not change:
+            instance.created_by = request.user
+        instance.save()
+
+
+class AlbumModelView(ModelView):
     pass
 
 
-class PhotoModelView(modelview.ModelView):
+class PhotoModelView(ModelView):
     pass
